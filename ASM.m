c130c202675fb04b5c977d0a477d8b246d58ad6f
flyingwolfz <57349703+flyingwolfz@users.noreply.github.com>
2021-10-05 17:11:33 +0800

    Update ASM.m

--- ASM.m
+++ ASM.m
@@ -1,6 +1,5 @@
 %https://github.com/flyingwolfz/angular-spectrum-method
 %angular spectrum method of fast calculation of diffraction 角谱衍射计算
-%shift:fftshift 要不要加fftshift
 %cut:output is set as the same size of input 要不要剪裁为原图大小
 %direction:direction of the propagation 传播方向
 %bandlimit:band-limit ASM 是否用带限角谱
@@ -10,11 +9,9 @@
 %z:distance 距离
 %pitch: pixel size 像素大小
 %lambda: wavelength 波长
-function[final] = ASM(shift,cut,direction,bandlimit,quan,mu,z,pitch,lambda)
+function[final] = ASM(cut,direction,bandlimit,quan,mu,z,pitch,lambda)
 
 [kuan,chang] = size(quan);
-% pitch=8*10^(-3);
-% lambda=638*10^(-6);
 a=mu*kuan;
 b=mu*chang;
 u0=1/b/pitch;
@@ -35,11 +32,9 @@ if(strcmp(bandlimit,'limit'))
     trans(abs(x)>xlimit)=0;
     trans(abs(y)>ylimit)=0;
 end
-if(strcmp(shift,'shift'))
-    final=ifft2(trans.*fftshift(fft2(quan2)));
-else
-    final=ifft2(trans.*(fft2(quan2)));
-end
+
+final=fftshift(ifft2(fftshift(trans.*fftshift(fft2(fftshift(quan2))))));
+
 if(strcmp(cut,'cut'))
  cutfinal=final(1+a/2-kuan/2:a/2+kuan/2,1+b/2-chang/2:b/2+chang/2);
  final=cutfinal;
